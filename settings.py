02da218464dc42c2ca8fec31d89b63e0a8a20056
jaanhunzai <jaanhunzai.512@gmail.com>
2020-09-20 13:50:48 +0200

    new test file
    uploaded new file

---
+++ settings.py
@@ -0,0 +1,18 @@
+#settings.py
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 15:51:54 2018
+
+@author: s_jan001
+"""
+
+import os
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   #
+OUTPUT_DIR = os.path.join(APP_ROOT,"output")
+#ANLYZER_PATH = os.path.join(OUTPUT_DIR,"..")
+DIR_QCNS = "./output"
+DIR_DATA = "./data"
+metricMapID =None
+sketchMapID= None
